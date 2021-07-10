0a5f29a241abe9517ac174ef02c3fbc682917d42
cate6495 <sandacate@gmail.com>
2021-07-10 10:21:18 +0300

    commit

--- model.py
+++ model.py
@@ -5,8 +5,9 @@ from sklearn.model_selection import train_test_split
 
 data = pd.read_csv("Salary.csv")
 
-x = data.iloc[:, :1].values
-y = data.iloc[:, 1:].values
+x = data[['YearsExperience']]
+y = data['Salary']
+
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
